a0c2f913f66b0ac0aa30ea4c4e5ad87c133eca8b
codethread <adamhalldesigns@gmail.com>
2024-06-11 07:50:33 +0100

    lint tidy

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,11 +7,12 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', '*.typegen.ts'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['off'],
   },
 };
