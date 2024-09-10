f35fd6d43791ca2d9c3fa056fcf5125ddc264a70
codethread <adamhalldesigns@gmail.com>
2024-09-10 10:58:36 +0100

    feat: store running status

--- client/src/integrations/stats.ts
+++ client/src/integrations/stats.ts
@@ -3,15 +3,21 @@ import { TimerHooks } from '@shared/types';
 
 export const statHooks: TimerHooks = {
   onTickHook() {},
-  onStartHook: () => {},
+  onStartHook: ({ bridge }) => {
+    bridge.storeUpdate({ status: { running: true } });
+  },
   onPauseHook: () => {},
   onPlayHook: () => {},
   onStopHook: ({ bridge, config, timer }) => {
+    bridge.storeUpdate({ status: { running: false } });
+
     if (timer.type === 'pomo') {
       bridge.statsTimerComplete(config.timers.pomo * 60 - toSeconds(timer));
     }
   },
   onCompleteHook: ({ bridge, timer }) => {
+    bridge.storeUpdate({ status: { running: false } });
+
     if (timer.type === 'pomo') {
       bridge.statsTimerComplete(timer.target * 60);
     }
